052a6ed2554c23d7910ba6df772b8d9fb07adf9a
Tanchohang A. Limbu <tanchohang@gmail.com>
2018-06-21 19:24:07 +0545

    bootstrap 4 logic for production

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <script src="{{asset('js/app.js')}}"></script>
-    @if(getenv('APP_ENV')=="production")
+    @if(config('app.env')=="production")
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     @endif
     <script src="http://cdn.bootcss.com/toastr.js/latest/js/toastr.min.js"></script>
